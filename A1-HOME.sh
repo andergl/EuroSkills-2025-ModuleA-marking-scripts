0fc46b3c88ab6b3b1993f4a9e91cccdcd5bafb8e
andergl <52236484+andergl@users.noreply.github.com>
2025-08-26 16:15:23 +0200

    Update A2-HOME.sh

--- A1-HOME.sh
+++ A1-HOME.sh
@@ -20,7 +20,7 @@ NC='\033[0m'
 echo ""
 echo ""
 echo -e $BLUE"######################################################################################"
-echo "Marking A1-HOME"
+echo "Marking A2-HOME"
 echo -e "######################################################################################"$NC
 echo ""
 echo ""
@@ -30,6 +30,71 @@ pause 'Press [ENTER] key to continue...'
 clear
 echo ""
 
+################## Per aspect section ##################
+echo ""
+echo ""
+echo -e $PURPLE"######################################################################################"
+echo "A1 General Configuration"
+echo -e "######################################################################################"$NC
+echo ""
+
+	if [  $( hostname  | grep -ic "HOME") = 1 ]
+	then  
+		 echo -e $GREEN"OK - Check hostname"$NC
+	else
+		 echo -e $RED"FAILED - Check hostname"$NC
+			echo "-----------------------------------------------------------------"
+			hostname
+				echo "-----------------------------------------------------------------"
+				echo -e $YELLOW"Correct hostname is: HOME"$NC
+	fi
+
+	if [  $( ip a | grep "inet.*global" | grep -ic "203.0.114.50/25") = 1 ] 
+	then  
+		 echo -e $GREEN"OK - Check ip address"$NC
+	else
+		 echo -e $RED"FAILED - Check ip address"$NC
+			echo "-----------------------------------------------------------------"
+			ip a | grep "inet.*global"
+				echo "-----------------------------------------------------------------"
+				echo -e $YELLOW"Must contain 203.0.114.50/25"$NC
+	fi	
+
+	if [  $( timedatectl | grep -i "zone" | grep -ic "Europe/Copenhagen" ) = 1 ]
+	then  
+		echo -e $GREEN"OK - Check Timezone"$NC
+	else
+		echo -e $RED"FAILED - Check Timezone"$NC
+			echo "-----------------------------------------------------------------"
+			timedatectl | grep -i "zone"
+				echo "-----------------------------------------------------------------"
+				echo -e $YELLOW"Time zone: Europe/Copenhagen"$NC
+	fi
+ 
+	if [  $( cat /etc/default/keyboard | grep XKBLAYOUT | grep -ic "us" ) = 1 ]
+	then  
+		 echo -e $GREEN"OK - Check keyboard"$NC
+	else
+		 echo -e $RED"FAILED - Check keyboard"$NC
+	fi
+
+	#if [  $( echo $LANG  | grep -ic "en_US.UTF-8" ) = 1 ]
+	#then  
+	#	 echo -e $GREEN"OK - Check Language"$NC
+	#else
+	#	 echo -e $RED"FAILED - Check Language"$NC
+	#		echo "-----------------------------------------------------------------"
+	#		echo $LANG
+	#			echo "-----------------------------------------------------------------"
+	#			echo -e $YELLOW"en_US.UTF-8"$NC
+	#fi
+
+
+echo -e $YELLOW"If every item before is GREEN, point for first aspect."$NC
+echo ""
+pause 'Press [ENTER] key to continue...'
+clear
+echo ""
 
 
 
